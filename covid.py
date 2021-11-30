628998a0409bd3eed6957fc842518f48278b7d5e
Christian <christiangoergen@gmail.com>
2021-11-30 21:45:53 +0100

    Covid data plot

--- covid.py
+++ covid.py
@@ -2,6 +2,33 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_excel('data/Fallzahlen_Kum_Tab.xlsx',index_col=0)
+# Importing data from https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Ausbruchsdaten.html
+data = pd.read_excel('data/Ausbruchsdaten.xlsx',index_col=3)
 df = pd.DataFrame(data)
-print (df.head())
+
+# Removing data from 2020 and picking the dataset of current week
+
+df_cleaned = df[df.Meldejahr != 2020]
+df_cleaned = df[df.Meldewoche >= 53]
+
+# Dropping "Meldejahr" and "sett_f" columns
+df_cleaned = df_cleaned.drop('Meldejahr', 1)
+df_cleaned = df_cleaned.drop('sett_f', 1)
+
+# Sorting
+infections = df_cleaned.sort_values(by=['n'], ascending=True)
+
+# Plot
+x = infections.index
+y = infections['n']
+
+plt.rcParams.update({'figure.autolayout': True})
+
+fig, ax = plt.subplots()
+plt.style.use('_mpl-gallery')
+
+ax.barh(x, y)
+ax.set_xlabel('Number of confirmed cases')
+ax.set_title('Where do people caught covid last week?')
+
+plt.show()
