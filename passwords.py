77bb6f14a063170cba35be73887a56300c9b3d0a
Christian <christiangoergen@gmail.com>
2021-12-16 16:27:27 +0100

    Own function added

--- passwords.py
+++ passwords.py
@@ -13,6 +13,41 @@ missing_values_count = df.isnull().sum()
 
 df = df.drop(columns=['Global_rank','Time_to_crack'])
 
+# Create new column "Password Length"
+df['Password Length'] = df['Password'].apply(len)
+
+print(df.columns)
+
+'''
+x = df['User_count'].head(50)
+y = df['Time_to_crack_in_seconds'].head(50)
+
+plt.scatter(x,y)
+plt.show()
+'''
+
+
+def cc(code):
+    """
+    This Function to extract most popular passwords by country code
+  """
+    code = df.loc[(df['country_code'] == code)]
+    code = code.drop(columns=['country', 'country_code', 'Rank', 'Time_to_crack_in_seconds', 'Password Length'])
+    print(code)
+
+it = cc('it')
+fr = cc('fr')
+de = cc('de')
+gb = cc('gb')
+ru = cc('ru')
+
+# Add some bar charts showing top pw per country
+
+
+#de = []
+#for x in df['country_code'] == 'de':
+#   de.append(x)
+
 
 # Merging DataFrames
 
