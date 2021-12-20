71af2e87b99a38f3872c4afb1eba99038981799e
Christian <christiangoergen@gmail.com>
2021-12-20 14:55:38 +0100

    Cleaning

--- classification.py
+++ classification.py
@@ -7,7 +7,7 @@ features = df[['Time_to_crack_in_seconds','Password Length']]
 
 X = StandardScaler().fit_transform(features)
 
-#initialise k_means for each k
+# Initialise k_means
 squared_distances = []
 K = range(1,15)
 for k in K:
@@ -16,7 +16,7 @@ for k in K:
     squared_distances.append(k_means.inertia_)
 
 
-#Plotting sum_of_squared
+# Plotting sum_of_squared
 '''
 plt.plot(K, squared_distances,marker='v')
 plt.xlabel('k')
@@ -25,7 +25,7 @@ plt.title('Check for an elbow to identify the best number of clusters')
 plt.show()
 '''
 
-# 3 gives us the best result
+# 3 gives us the best result according to the plot (elbow method)
 kmeans_3 = KMeans(n_clusters=3)
 model = kmeans_3.fit(X)
 predict = kmeans_3.predict(X)
@@ -35,7 +35,6 @@ predict = kmeans_3.predict(X)
 df['Difficulty'] = pd.DataFrame(predict, index=df.index)
 
 # Create dfs with cat1, cat2 and cat3 pw
-
 pwcat1 = df.loc[(df['Difficulty'] == 0)]
 pwcat1 = pwcat1.drop(columns=['country', 'country_code', 'Rank'])
 
@@ -47,4 +46,3 @@ pwcat3 = pwcat3.drop(columns=['country', 'country_code', 'Rank'])
 
 # Create dictionary
 passwords = {'Cat1':list(pwcat1['Password']),'Cat2':list(pwcat2['Password']),'Cat3':list(pwcat3['Password'])}
-
