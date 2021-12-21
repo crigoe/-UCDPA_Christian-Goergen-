a4f28526d6db5478e2bd3b7f5a89d31278882cbd
Christian <christiangoergen@gmail.com>
2021-12-21 16:35:12 +0100

    Update

--- pw-unsupervised.py
+++ pw-unsupervised.py
@@ -1,6 +1,9 @@
 from pw_dataframes import *
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
 
 # Features to create classification for password difficulty
 features = df[['Time_to_crack_in_seconds','Password Length']]
@@ -47,4 +50,41 @@ pwcat3 = pwcat3.drop(columns=['country', 'country_code', 'Rank'])
 # Create dictionary
 passwords = {'Cat1':list(pwcat1['Password']),'Cat2':list(pwcat2['Password']),'Cat3':list(pwcat3['Password'])}
 
-#Print or visualize Clusters
+# Create lists
+passwords1 = list(passwords.values())[0]
+passwords2 = list(passwords.values())[1]
+passwords3 = list(passwords.values())[2]
+
+
+#Print cluster 1
+
+print (passwords1)
+wordcloud = WordCloud().generate(" ".join(passwords1))
+# Display the generated image:
+plt.figure(figsize=(10,7))
+plt.title('Password Cluster 1')
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+#Print cluster 2
+
+print (passwords2)
+wordcloud = WordCloud().generate(" ".join(passwords2))
+# Display the generated image:
+plt.figure(figsize=(10,7))
+plt.title('Password Cluster 2')
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+#Print cluster 3
+
+print (passwords3)
+wordcloud = WordCloud().generate(" ".join(passwords3))
+# Display the generated image:
+plt.figure(figsize=(10,7))
+plt.title('Password Cluster 3')
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
