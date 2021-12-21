4a3a2fd67c67d0cfec968545fb2ec76724a88114
Christian <christiangoergen@gmail.com>
2021-12-21 08:04:30 +0100

    Update

--- Supervised.py
+++ Supervised.py
@@ -2,25 +2,28 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 #Loading dataset
 data = pd.read_csv('data/DAX.csv')
 df = pd.DataFrame(data)
 
-#Translating column names
+#Translating column names from DE to EN
 df = df.rename(columns={"Datum": "Date", "Zuletzt": "Close", "Eröffn.": "Open", "Hoch": "High", "Tief": "Low", "Vol": "Vol", "+/- %": "Change"})
 
+#Set Column index
 df = df.set_index("Date")
-df['Change'] = df['Change'].str.replace(',','.')
 
+#Create new column containing information wether the price went up or down
+
+df['Change'] = df['Change'].str.replace(',','.')
 dailychange = (df['Change'])
 dailychange = [x.strip('%') for x in dailychange]
 
-# Add new column to dataframe
 se = pd.Series(dailychange)
 df['dailychange'] = se.values
 df['dailychange'] = df['dailychange'].astype(float)
-
 
 upordown = df['dailychange'] > 0
 
@@ -31,11 +34,10 @@ df['Up Or Down'] = df['Up Or Down'].str.replace('False','0')
 
 pd.concat([df, df['Up Or Down']], axis=1)
 
+# Dropping necessary and helper columns
 cleaned_df = df.drop(columns=['Vol.','Change','dailychange'])
 
-x=cleaned_df[['Close', 'Open', 'High','Low']]
-y=cleaned_df['Up Or Down']
-
+#Removing ",0" and ",5" from each column
 cleaned_df['Close'] = cleaned_df['Close'].str.replace(',0','')
 cleaned_df['Close'] = cleaned_df['Close'].str.replace(',5','')
 
@@ -48,21 +50,17 @@ cleaned_df['High'] = cleaned_df['High'].str.replace(',5','')
 cleaned_df['Low'] = cleaned_df['Low'].str.replace(',0','')
 cleaned_df['Low'] = cleaned_df['Low'].str.replace(',5','')
 
-
+#Converting entire DataFrame into numeric values
 cleaned_df = cleaned_df.apply(pd.to_numeric, errors='coerce')
 
-
-# Import KNeighborsClassifier from sklearn.neighbors
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.model_selection import train_test_split
-
+x=cleaned_df[['Close', 'Open', 'High','Low']]
+y=cleaned_df['Up Or Down']
 
 # Check for Missing Values and Duplicates
 missing_values_count2 = cleaned_df.isnull().sum()
 
 #Fill missing values with previos values
 cleaned_df = cleaned_df.fillna(method='ffill')
-
 
 # Create arrays for the features and the response variable
 y = cleaned_df['Up Or Down'].values
@@ -125,5 +123,3 @@ knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X_train,y_train)
 
 print('The accuracy is:',knn.score(X_test, y_test))
-
-
